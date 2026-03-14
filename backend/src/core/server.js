83731c0edb614889267a0a21d564dd80db7e54a6
Hrudhayasankalp <sankalphrudhaya@gmail.com.com>
2026-03-14 23:51:32 +0530

    Add request logging for troubleshooting 404 error

--- backend/src/core/server.js
+++ backend/src/core/server.js
@@ -15,6 +15,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Request Logging
+app.use((req, res, next) => {
+  console.log(`📡 ${req.method} ${req.url}`);
+  next();
+});
+
 connectDB();
 //app.use("/api", routes);
 
